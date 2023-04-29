d2a08c9879f553bcf84672e5b097441eecc97bea
domsdoms <dominicpunladomingo120@gmail.com>
2023-04-29 23:51:04 +0800

    #commit

--- index.js
+++ index.js
@@ -11,17 +11,12 @@ window.addEventListener("load", function () {
 });
 if (window.matchMedia("(max-width: 768px)").matches) {
   var msgBtn = document.querySelector(".msgUs-btn");
-  const navlist = document.querySelector(".nav-list");
-  msgBtn.innerHTML = ` <a href="mailto:algofilipinoofficers@gmail.com">   <i class="bi bi-chat" style="font-size: 30px; border: none; background-color: transparent"></i>  </a>`;
-  msgBtn.style.border = "none !important";
-}
 
-// const switchToggle = document.querySelector("#checkbox");
-// switchToggle.addEventListener("click", function () {
-//   window.addEventListener("click", function (event) {
-//     switchToggle.checked = false;
-//   });
-// });
+  msgBtn.style.display = "none";
+  // const navlist = document.querySelector(".nav-list");
+  // msgBtn.innerHTML = ` <a href="mailto:algofilipinoofficers@gmail.com">   <i class="bi bi-chat" style="font-size: 30px; border: none; background-color: transparent"></i>  </a>`;
+  // msgBtn.style.border = "none !important";
+}
 
 const scrollToTopButton = document.getElementById("scroll-to-top");
 window.addEventListener("scroll", () => {
@@ -46,26 +41,29 @@ const video = document.querySelector(".myVid");
 const list = lists.children;
 
 list.item(0).addEventListener("click", function () {
-  video.src = "Leadership.mp4";
+  video.src = `https://drive.google.com/file/d/1hsIDKdKepIgYuXZgUSu6bE1_b3pi_uHC/preview`;
 });
 var popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]')
 );
 list.item(1).addEventListener("click", function () {
-  video.src = "gdw.mkv";
+  video.src =
+    "https://drive.google.com/file/d/17B7f5bwCrYxhInOcKE66baYrJQ1jyHvE/preview";
 });
 var popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]')
 );
 
 list.item(2).addEventListener("click", function () {
-  video.src = "OSC.mov";
+  video.src =
+    "https://drive.google.com/file/d/1F2gp3PDcGu2QgJrHrXLZhzvk6TDAby2o/preview?autoplay=1";
 });
 var popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]')
 );
 list.item(3).addEventListener("click", function () {
-  video.src = "WMW.mp4";
+  video.src =
+    "https://drive.google.com/file/d/1LUYefC-mONY5IRbBdmFxf41LTu_oCmng/preview";
 });
 var popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]')
